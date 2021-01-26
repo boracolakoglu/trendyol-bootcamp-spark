7d76c83d26a94b13d55d33b77641bb8b98ef7cee
boracolakoglu <colakoglu.bora35@gmail.com>
2021-01-26 18:51:11 +0300

    update 1

--- src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
+++ src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
@@ -1,8 +1,44 @@
 package com.trendyol.bootcamp.homework
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+import org.apache.spark.sql.{Encoders, SaveMode, SparkSession}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
+
 object ProductMergerJob {
 
   def main(args: Array[String]): Unit = {
+
+    /** Read both json files as sql
+     *  Merge them togeher
+     *  Select id, name, category, brand, color, price, max(timestamp)
+     *  from mergedTable
+     *  gruop by id 
+     */
+  
+    import spark.implicits._
+
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("Spark HomeWork")
+      .getOrCreate()
+
+    val productsSchema = Encoders.product[Product].schema
+    val products = spark.read
+    .schema(productsSchema)
+    .json("data/homework/initial_data")
+    .as[Product]
+      
+
+    val changes = spark.read
+    .schema(productsSchema)
+    .json("data/homework/cdc_data")
+
+    val merged = ???
 
     /**
     * Find the latest version of each product in every run, and save it as snapshot.
